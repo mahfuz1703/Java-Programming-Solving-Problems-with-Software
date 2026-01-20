5ac209ad34976a743eb62076a6ec6516b55403b7
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2026-01-20 22:05:26 +0600

    Remove unused imports in CreateFileExample.java

--- Exception Handling & File IO/File_IO/CreateFileExample.java
+++ Exception Handling & File IO/File_IO/CreateFileExample.java
@@ -1,9 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 public class CreateFileExample {
     public static void main(String[] args) {
         // Path newFilePath = Paths.get("newfile.txt");
